29540a2857b31a3b7d231e9199b0d066680f7a66
yiting0418 <145081180+yiting0418@users.noreply.github.com>
2024-06-11 22:17:56 +0800

    Update about.php

--- public_html/about.php
+++ public_html/about.php
@@ -3,7 +3,7 @@
 <div class="container">
 <h3>關於</h3>
 <h5>此為網頁設計課程之期末作品，非正式銷售網頁</h5>
-<h5>原始碼及說明請參考<a href="https://www.youtube.com/">Github</a></h5>
+<h5>原始碼及說明請參考<a href="https://github.com/yiting0418/wp/tree/master/public_html">Github</a></h5>
 
 
 </div>
